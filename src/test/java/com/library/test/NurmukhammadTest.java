2cb5f97289c732068ec38d4dcb92ddd1487f02f2
NUR-ISLOM <tillahoja005@gmail.com>
2025-05-01 12:57:54 -0500

    login function is done

--- src/test/java/com/library/test/NurmukhammadTest.java
+++ src/test/java/com/library/test/NurmukhammadTest.java
@@ -1,4 +1,8 @@
 package com.library.test;
 
 public class NurmukhammadTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello team leader i finish my job");
+    }
 }
